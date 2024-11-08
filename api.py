4791ca60c9c5e91fa62e3ead247ca9f5d6cafafe
EranTimothy-dev <eranperera.dev@gmail.com>
2024-11-08 19:19:51 +0530

    removed unneccassary parts

--- api.py
+++ api.py
@@ -1,12 +1,17 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort, marshal_with
+from flask_restful import Api, Resource, reqparse, abort, marshal_with, fields
 
 
 app = Flask(__name__)
 api = Api(app)
 
 
+video_fields = {
+    'id': fields.Integer,
+    'url': fields.String,
+}
 
+        
 
 
 
