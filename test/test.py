50cfa64dd19233e7b60ed9a98d62f994602aa3a9
EranTimothy-dev <eranperera.dev@gmail.com>
2024-11-09 13:52:09 +0530

    created a thread class that returns a value

--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+import threading
 import sys
 import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -21,7 +22,30 @@ age_restricted_video = "https://youtu.be/voQBX6yn2XY?si=e_4DHuE3jUDv5whc"
 # print(os.getcwd())
 # files = os.listdir(thumbnail_filepath)
 # print(files[0])
-process = download_video(url1)
+# process = download_video(url1)
+# for line in process.stdout:
+#     print(line.strip())
+
+# make a thread class with return value
+class ThreadWithReturnValue(threading.Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, *, daemon=None,
+                 verbose=None):
+        threading.Thread.__init__(self, group, target, name, args, kwargs, daemon=daemon)
+        self._return = None
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args, **self._kwargs)
+    def join(self, *args):
+        threading.Thread.join(self, *args)
+        return self._return
+
+
+t1 = ThreadWithReturnValue(target=download_video, args=(url1,))
+t1.start()
+process = t1.join()
 for line in process.stdout:
     print(line.strip())
+
+
 
