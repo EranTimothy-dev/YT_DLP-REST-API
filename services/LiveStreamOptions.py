2540e36ecde5ff9a227be79bf5a514078a33ff4b
EranTimothy-dev <eranperera.dev@gmail.com>
2024-12-25 18:55:56 +0530

    changed cookie access method

--- services/LiveStreamOptions.py
+++ services/LiveStreamOptions.py
@@ -4,7 +4,8 @@ import subprocess
 
 
 def download_live_stream(url):
-    cmd = ['yt-dlp',"--live-from-start","--cookies", "youtube.com_cookies.txt", url]
+    # cmd = ['yt-dlp',"--live-from-start","--cookies", "youtube.com_cookies.txt", url]
+    cmd = ['yt-dlp',"--live-from-start","--cookies-from-browser", "firefox", url]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, universal_newlines=True, bufsize=1)
     return process
     # subprocess.run(cmd, text = True)
