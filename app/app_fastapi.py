db2623d55aec963970b64d0602716302e71c3074
EranTimothy-dev <eranperera.dev@gmail.com>
2025-04-05 02:32:28 +0530

    refactored endpoint to controllers

--- app/app_fastapi.py
+++ app/app_fastapi.py
@@ -4,10 +4,6 @@ app = FastAPI()
 
 
 
-@app.get("/video")
-def extract_video_information():
-    pass
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- app/controllers/VideoController.py
+++ app/controllers/VideoController.py
@@ -1,3 +1,8 @@
 from app.app_fastapi import app
 from app.model.models import VideoInfo
 
+
+@app.get("/video")
+def extract_video_information():
+    pass
+
