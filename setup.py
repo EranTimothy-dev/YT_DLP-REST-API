371b1de41e5036fd2cad018961b01d2369998d6e
EranTimothy-dev <eranperera.dev@gmail.com>
2025-04-04 15:39:03 +0530

    added setup python script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="youtube_downloader", 
+    description="An REST-API service for downloading youtube videos and audios",
+    version="0.3.0",
+    packages=find_packages(),
+    author="Eran Perera", 
+    author_email="eranperera.dev@gmail.com"
+    )
