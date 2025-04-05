f5aa6e41cdeb4cf27273c5206260f12ae06eb988
EranTimothy-dev <eranperera.dev@gmail.com>
2025-04-05 23:34:01 +0530

    added models for requests

--- app/model/models.py
+++ app/model/models.py
@@ -1,8 +1,9 @@
 from typing import Optional
 from pydantic import BaseModel
+from uuid import UUID
 
 class VideoInfo(BaseModel):
-    url: str
+    id: Optional[UUID] = UUID
     title: str
     description: Optional[str] = None
     thumbnail: Optional[str] = None
@@ -10,3 +11,8 @@ class VideoInfo(BaseModel):
     like_count: Optional[str] = None
     duration: Optional[str] = None 
     available_resolutions: set
+
+
+class VideoRequest(BaseModel):
+    id: Optional[UUID] = UUID
+    url: str
