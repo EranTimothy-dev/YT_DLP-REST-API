c2ae097a1a4ac1ab25baf419bf7027b4cb533eee
EranTimothy-dev <eranperera.dev@gmail.com>
2025-04-12 23:34:42 +0530

    updated __init__.py files for services and models

---
+++ app/model/__init__.py
@@ -0,0 +1,9 @@
+"""
+Models Package
+
+This module contains all the models that are used in the application
+"""
+
+
+__version__ = "0.1.0"
+__author__ = "Eran Perera"
---
+++ app/services/__init__.py
@@ -0,0 +1,10 @@
+"""
+Services Package
+
+This module contains all the services that are used in the application for downloading
+youtube videos and Audios
+"""
+
+
+__version__ = "0.1.0"
+__author__ = "Eran Perera"
