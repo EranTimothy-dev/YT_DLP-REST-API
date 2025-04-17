3de482dec2a1c75eb1aea1e2ed59097b6da85cc0
EranTimothy-dev <eranperera.dev@gmail.com>
2025-04-18 02:48:14 +0530

    added logging

---
+++ app/middleware/logger.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+
+logger = logging.getLogger()
+
+
+formatter = logging.Formatter(
+    fmt='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d-%m-%Y %H:%M:%S'
+)
+
+stream_handler = logging.StreamHandler(sys.stdout)
+file_handler = logging.FileHandler('status.log')
+
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
