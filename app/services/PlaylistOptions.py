8ab2720b415c160195f94200f833f7ba9c56e7d1
EranTimothy-dev <eranperera.dev@gmail.com>
2025-04-18 03:29:38 +0530

    made playlist videos to download lazily

--- app/services/PlaylistOptions.py
+++ app/services/PlaylistOptions.py
@@ -15,13 +15,13 @@ def download_playlist_audio_selection(playlist_url:str, selection:str):
     # subprocess.run(cmd, capture_output=True, text = True)
 
 def download_playlist(playlist_url):
-    cmd = ['yt-dlp', playlist_url, "--yes-playlist", "-P", "playlist\\", "-P", "temp:temp\\","-f","bv*[height=720]+ba", "--merge-output-format", "mkv", "--windows-filenames"]
+    cmd = ['yt-dlp', playlist_url, "--yes-playlist","--lazy-playlist", "-P", "playlist\\", "-P", "temp:temp\\","-f","bv*[height=720]+ba", "--merge-output-format", "mkv", "--windows-filenames"]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, universal_newlines=True, bufsize=1, encoding="utf-8")
     return process
     # subprocess.run(cmd, text = True)
 
 def download_playlist_audio(playlist_url):
-    cmd = ['yt-dlp', playlist_url, "--yes-playlist", "--extract-audio", "--audio-format", "mp3"]
+    cmd = ['yt-dlp', playlist_url, "--yes-playlist","--lazy-playlist", "--extract-audio", "--audio-format", "mp3"]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, universal_newlines=True, bufsize=1, encoding="utf-8")
     return process
     # subprocess.run(cmd, capture_output=True, text = True)
