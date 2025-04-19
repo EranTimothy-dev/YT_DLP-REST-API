603dd3e9e7e50deb383e68f8bb767de3abe2ad52
EranTimothy-dev <eranperera.dev@gmail.com>
2025-04-19 22:08:06 +0530

    fixed asynchronous running of download and stop download methods

--- app/app_fastapi.py
+++ app/app_fastapi.py
@@ -2,11 +2,14 @@ from fastapi import  FastAPI
 from fastapi.responses import ORJSONResponse
 from typing import Optional
 from subprocess import Popen
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
 
 from app.schemas.schema import VideoRequest, VideoResponse, DownloadRequest
 from app.services.ThreadRV import ThreadWithReturnValue
 from app.services.downloadHandler import get_information
 from app.services.DownloadOptions import download_video
+
 
 app = FastAPI(
     title="Youtube Downloader",
@@ -27,20 +30,25 @@ async def extract_info(request: VideoRequest) -> VideoResponse:
 @app.post("/download_video")
 async def download_yt_video(request: Optional[DownloadRequest]) -> dict:
     global download
-    download = download_video(request.url, request.quality, request.extension)
-    for line in download.stdout:
-        print(f"\r{line.strip():<150}", end="",flush=True) # make sure the progress is printied on the same line
-    print("\n Download Completed!")
-    return {"ok": True}
+
+    def downloader():
+        global download
+        download = download_video(request.url, request.quality, request.extension)
+        for line in download.stdout:
+            print(f"\r{line.strip():<150}", end="",flush=True) # make sure the progress is printied on the same line
+
+    await asyncio.to_thread(downloader)
+    return {200: "Download completed!"}
 
 @app.post("/stop_download")
 async def stop_download():
-    if isinstance(download,Popen[str]):
+    global download
+    if isinstance(download,Popen):
         download.terminate()
         print("\n Download Stopped!")
-        return {"ok": True}
+        return {200: "Download Stopped!"}
     elif download is None:
-        return {"ok": False}
+        return {406: "No download runnning"}
     
 
 @app.post("/test")
