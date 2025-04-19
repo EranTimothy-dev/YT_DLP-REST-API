512347885b42459aa943c6e668683827e3bc05a4
EranTimothy-dev <eranperera.dev@gmail.com>
2025-04-20 03:45:46 +0530

    fixed websocket issue, and blocking I/O

--- app/app_fastapi.py
+++ app/app_fastapi.py
@@ -1,9 +1,11 @@
-from fastapi import  FastAPI, Response, status, WebSocket
+from fastapi import  FastAPI, Response, status, WebSocket, WebSocketDisconnect
 from fastapi.responses import ORJSONResponse, HTMLResponse
 from typing import Optional
 from subprocess import Popen
 from signal import SIGTERM
+import signal
 import asyncio
+import psutil
 
 from app.schemas.schema import VideoRequest, VideoResponse, DownloadRequest
 from app.services.downloadHandler import get_information
@@ -30,25 +32,42 @@ async def extract_info(request: VideoRequest) -> VideoResponse:
 async def websocket_endpoint(websocket: WebSocket):
     global download
     await websocket.accept()
-    while True:
-        if download is not None:
+    try:
+        while True:
+            # if download is not None:
             message = await progress_queue.get()
             await websocket.send_text(f"{message}")
+    except WebSocketDisconnect:
+        print("Client disconnected")
+    except asyncio.exceptions.CancelledError:
+        print("Force Cancelled Client Connection")
 
 
+loop = asyncio.get_event_loop()
 
 @app.post("/download_video")
 async def download_yt_video(request: Optional[DownloadRequest], response: Response) -> dict:
     global download
+    
 
     def downloader():
+        global loop
         global download
         download = download_video(request.url, request.quality, request.extension)
         for line in download.stdout:
             if download.returncode == SIGTERM:
+                print("Stopping Download")
+                try:
+                    download.terminate()
+                except RuntimeError:
+                    print("Runtime error occurred")
+                except Exception as e:
+                    print(f"Error occurred: {str(e)}")
                 return
-            asyncio.run(progress_queue.put(line))
+            # asyncio.run(progress_queue.put(line))
+            loop.call_soon_threadsafe(progress_queue.put_nowait, line)
             print(f"\r{line.strip():<150}", end="",flush=True) # make sure the progress is printied on the same line
+        download.wait()
 
     await asyncio.to_thread(downloader)
     if download.returncode is None:
@@ -61,10 +80,23 @@ async def download_yt_video(request: Optional[DownloadRequest], response: Respon
 @app.post("/stop_download")
 async def stop_download(response: Response):
     global download
+    # global loop
     if isinstance(download,Popen):
+        try:
+            parent = psutil.Process(download.pid)
+            children = parent.children(recursive=True)
+            for child in children:
+                child.kill()
+            parent.kill()
+            print("Killed process and its children.")
+        except Exception as e:
+            print(f"Error killing process tree: {e}")
         response.status_code = status.HTTP_200_OK
         download.returncode = SIGTERM
-        download.terminate()
+        # download.send_signal(signal.CTRL_BREAK_EVENT)
+        # download.terminate()
+        # download.kill()
+        # loop.stop()
         print("\n Download Stopped!")
         return {response.status_code: "Download Stopped!"}
     elif download is None:
