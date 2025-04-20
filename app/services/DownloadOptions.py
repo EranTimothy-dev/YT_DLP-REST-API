ccf92f4c87780d84a0df3015d2769add9ec841c9
EranTimothy-dev <eranperera.dev@gmail.com>
2025-04-20 23:54:54 +0530

    modified download methods to start as a new process

--- app/services/DownloadOptions.py
+++ app/services/DownloadOptions.py
@@ -4,7 +4,7 @@ from subprocess import CREATE_NEW_PROCESS_GROUP
 
 def download_audio(url):
     cmd = ['yt-dlp', url, "--extract-audio", "--audio-format", "mp3", "-P", "downloads\\", "-P", "temp:temp\\","--windows-filenames"]
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, universal_newlines=True, bufsize=1, encoding="utf-8")
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, universal_newlines=True, bufsize=1, encoding="utf-8", creationflags=CREATE_NEW_PROCESS_GROUP)
     return process
     # subprocess.run(cmd, capture_output=True, text = True)
 
@@ -17,7 +17,7 @@ def download_video(url,quality,extension):
 
 def download_age_restricted_video(url, quality, extension):
     cmd = ['yt-dlp',"--cookies-from-browser", "firefox", url,"-f", f"bv*[height={quality}]+ba","--merge-output-format", f"{extension}", "-P", "downloads\\", "-P", "temp:temp\\","--windows-filenames","-N", "4"]
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, universal_newlines=True, bufsize=1, encoding="utf-8")
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, universal_newlines=True, bufsize=1, encoding="utf-8", creationflags=CREATE_NEW_PROCESS_GROUP)
     return process
     
 
