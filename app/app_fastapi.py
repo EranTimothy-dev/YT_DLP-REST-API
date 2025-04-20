0c9be2b6441780b5e0959b44faf430da3d30c230
EranTimothy-dev <eranperera.dev@gmail.com>
2025-04-20 18:28:15 +0530

    refactor: removed test code and unused snippets

--- app/app_fastapi.py
+++ app/app_fastapi.py
@@ -53,19 +53,8 @@ async def download_yt_video(request: Optional[DownloadRequest], response: Respon
         global download
         download = download_video(request.url, request.quality, request.extension)
         for line in download.stdout:
-            # backup termination code snippet
-            # if download.returncode == SIGTERM:
-            #     print("Stopping Download")
-            #     try:
-            #         download.terminate()
-            #     except RuntimeError:
-            #         print("Runtime error occurred")
-            #     except Exception as e:
-            #         print(f"Error occurred: {str(e)}")
-            #     return
             loop.call_soon_threadsafe(audio_progress_queue.put_nowait, line)
             print(f"\r{line.strip():<150}", end="",flush=True) # make sure the progress is printied on the same line
-        # download.wait()
         download.returncode = None
 
     # run sync function asynchronously as a thread
@@ -99,49 +88,6 @@ async def stop_download(response: Response):
         response.status_code = status.HTTP_406_NOT_ACCEPTABLE
         return {response.status_code: "No download runnning"}
     
-
-@app.post("/test")
-async def test_endpoint(request: dict):
-    return {"ok": True}
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
 
 if __name__ == "__main__":
     import uvicorn
